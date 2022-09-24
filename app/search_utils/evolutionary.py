5119f683866cb01cba3f12661eb24da742748904
Armando Ortiz Peñafiel <armandopenafiel12@gmail.com>
2022-09-23 22:08:45 -0500

    add functionality - custom method
    The user could include the desired method. First send the code to execute and after that create the evolutionary algorithm.

--- app/search_utils/evolutionary.py
+++ app/search_utils/evolutionary.py
@@ -10,6 +10,20 @@ import pyristic.utils.helpers as pc_utils
 import argument_types as arg_api
 import utils
 
+def get_evolutionary_method(operator_type, config):
+    """
+    Description:
+        This method helps to obtain the method of the right module. It could be
+        two options. The first option is a custom method uploaded to the server or the second
+        option  is a official pyristic method.
+    """
+    method_name = config[operator_type].operator_name
+    if method_name != 'CustomMethod':
+        search_location = pc_method
+        if operator_type == 'setter_invalid_solution':
+            search_location = pc_utils
+        return getattr(search_location, method_name)
+    return utils.ModulesHandler().get_method_by_module(operator_type, 'CustomMethod')
 
 def create_evolutionary_config(
     algorithm_type: arg_api.EvolutionaryAlgorithm,
@@ -44,15 +58,10 @@ def create_evolutionary_config(
         required_keys += [
             'adaptive_mutation_operator'
         ]
-
     pyristic_config = OptimizerConfig()
     for operator_type in required_keys:
         try:
-            search_location = pc_method
-            if operator_type == 'setter_invalid_solution':
-                search_location = pc_utils
-            method = getattr(search_location, config[operator_type].operator_name)
-
+            method = get_evolutionary_method(operator_type, config)
             try:
                 pyristic_config.methods[operator_type] = method(*config[operator_type].parameters)
             except:
